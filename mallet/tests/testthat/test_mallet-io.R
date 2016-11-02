2531db31715fcbe74810872cc1f885ab2c6ec3aa
Måns Magnusson <mons.magnusson@gmail.com>
2016-11-02 14:22:36 -0400

    Updated test suite to include both identified bugs

--- mallet/tests/testthat/test_mallet-io.R
+++ mallet/tests/testthat/test_mallet-io.R
@@ -10,10 +10,13 @@ sotu.instances <-
                 token.regexp = "\\p{L}[\\p{L}\\p{P}]+\\p{L}")
 topic.model <- MalletLDA(num.topics=10, alpha.sum = 1, beta = 0.1)
 topic.model$loadDocuments(sotu.instances)
-topic.model$train(20)
+topic.model$setAlphaOptimization(10, 10)
+topic.model$train(100)
 
 old.doctopic <- mallet.doc.topics(topic.model, smoothed=FALSE, normalized=FALSE)
 old.topictype <- mallet.topic.words(topic.model, smoothed=FALSE, normalized=FALSE)
+old.doctopic.prior <- mallet.doc.topics(topic.model, smoothed=TRUE, normalized=FALSE)
+old.topictype.prior <- mallet.topic.words(topic.model, smoothed=TRUE, normalized=FALSE)
 
 state_file <- file.path(tempdir(), "temp_mallet_state.gz")
 wrong_state_file <- file.path(tempdir(), "temp_mallet_state.txt")
@@ -38,6 +41,8 @@ test_that(desc="load.mallet",{
   new.topic.model$loadDocuments(sotu.instances)
   new.doctopic.before.load <- mallet.doc.topics(new.topic.model, smoothed=FALSE, normalized=FALSE)
   new.topictype.before.load <- mallet.topic.words(new.topic.model, smoothed=FALSE, normalized=FALSE)
+  new.doctopic.before.load.prior <- mallet.doc.topics(new.topic.model, smoothed=TRUE, normalized=FALSE)
+  new.topictype.before.load.prior <- mallet.topic.words(new.topic.model, smoothed=TRUE, normalized=FALSE)
   
   expect_true(file.exists(state_file))
 
@@ -48,16 +53,26 @@ test_that(desc="load.mallet",{
   
   new.doctopic.after.load <- mallet.doc.topics(new.topic.model, smoothed=FALSE, normalized=FALSE)
   new.topictype.after.load <- mallet.topic.words(new.topic.model, smoothed=FALSE, normalized=FALSE)
+  new.doctopic.after.load.prior <- mallet.doc.topics(new.topic.model, smoothed=TRUE, normalized=FALSE)
+  new.topictype.after.load.prior <- mallet.topic.words(new.topic.model, smoothed=TRUE, normalized=FALSE)
 
+  
+  skip_on_travis() # This is probably a bug in Mallet RTopicModel class
   expect_equal(new.topictype.after.load, old.topictype)
+  expect_equal(new.topictype.after.load.prior, old.topictype.prior)
   expect_true(any(new.topictype.after.load != new.topictype.before.load))
+  expect_true(any(new.topictype.after.load.prior != new.topictype.before.load.prior))
+  
   expect_equal(680947, sum(old.topictype))
   expect_equal(680947, sum(new.topictype.after.load))
   expect_equal(680947, sum(new.topictype.before.load))
   
-  skip_on_travis() # This is probably a bug in Mallet RTopicModel class
+  
   expect_equal(new.doctopic.after.load, old.doctopic)
+  expect_equal(new.doctopic.after.load.prior, old.doctopic.prior)
   expect_true(any(new.doctopic.after.load != new.doctopic.before.load))
+  expect_true(any(new.doctopic.after.load.prior != new.doctopic.before.load.prior))
+
   expect_equal(680947, sum(old.doctopic))
   expect_equal(680947, sum(new.doctopic.before.load))
   expect_equal(680947, sum(new.doctopic.after.load))
