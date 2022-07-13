9fe22fb080af47b120a814bf70ad0f19dc1cb9cf
Måns Magnusson <mons.magnusson@gmail.com>
2022-07-13 18:49:18 +0200

    fixes

--- mallet/tests/testthat/test_mallet_deprecated.R
+++ mallet/tests/testthat/test_mallet_deprecated.R
@@ -4,10 +4,9 @@ data(sotu)
 
 test_that(desc="mallet.read.dir",{
   skip_on_cran()
-  test_folder <- file.path(tempdir(), "test_mallet_read_dir")
-  dir.create(test_folder, showWarnings = FALSE)
+  directory <- system.file("stoplists", package = "mallet")
 
-  expect_warning(mallet::mallet.read.dir(test_folder), regexp = "deprecated")
+  expect_warning(mallet::mallet.read.dir(directory), regexp = "deprecated")
 
 })
 
